9951b87c050c96fbb73167ece398b09e8ae1956f
TUSHAR MISHRA <mishratushar8890@gmail.com>
2024-03-25 20:39:49 +0530

    added blog view page

--- lib/features/blog/presentation/widgets/blog_card.dart
+++ lib/features/blog/presentation/widgets/blog_card.dart
@@ -1,5 +1,6 @@
 import 'package:blogster/core/utils/read_time.dart';
 import 'package:blogster/features/blog/domain/entities/blog.dart';
+import 'package:blogster/features/blog/presentation/pages/blog_view.dart';
 import 'package:flutter/material.dart';
 
 class BlogCard extends StatelessWidget {
@@ -14,7 +15,14 @@ class BlogCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+          context,
+          BlogViewerPage.route(
+            blog,
+          ),
+        );
+      },
       child: Container(
         height: 200,
         margin: const EdgeInsets.all(16).copyWith(
